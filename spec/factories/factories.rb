6fb9b70c6521893bb00c71722738d7bd5658a244
Anna Weisbrodt <aweisbro@gmail.com>
2016-09-12 12:09:17 -0600

    edit link feature in progress:

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -3,4 +3,15 @@ FactoryGirl.define do
     email "bob@gmail.com"
     password "password"
   end
+
+  sequence :title do |n|
+    "Title#{n}"
+  end
+
+  factory :link do
+    title
+    url "www.google.com"
+    user_id 1
+  end
+
 end
--- spec/features/authenticated_user_edits_link_spec.rb
+++ spec/features/authenticated_user_edits_link_spec.rb
@@ -1,18 +1,19 @@
 require "rails_helper"
 
-feature "authenticated user submits link" do
-  scenario "user sees all user's links on index load" do
+feature "authenticated user edits link" do
+  scenario "user sees updated link in index" do
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return( user )
-    create_list(:link, 3)
 
     visit links_path
 
-    within("#1") do
-      click_on "Edit"
-    end
+
+    fill_in "Title", with: "NYT"
+    fill_in "Url", with: "www.nytimes.com"
+    click_on "Submit Link"
 
     expect(page).to have_content "NYT"
     expect(page).to have_content "www.nytimes.com"
+
   end
 end
