1eb3139fe7aef33105a146861f05781788de2205
Anna Weisbrodt <aweisbro@gmail.com>
2016-09-20 14:33:51 -0600

    add to edit status spec

--- spec/features/user_edits_status_spec.rb
+++ spec/features/user_edits_status_spec.rb
@@ -3,16 +3,21 @@ require "rails_helper"
 feature "user edits link status" do
   scenario "user sees updated status", js: true do
     user = create(:user)
-    target_link = create(:link, user_id: user.id)
-    other_link = create(:link, user_id: user.id)
+    read_link = create(:link, read: "true", user_id: user.id)
+    unread_link = create(:link, user_id: user.id)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return( user )
 
     visit links_path
 
-    within("ul#link#{target_link.id}") do
+    within("ul#link#{unread_link.id}") do
       expect(page).to have_content("Mark As Read")
-      find("button#button#{target_link.id}")
+      find("button#button#{unread_link.id}")
+    end
+
+    within("ul#link#{read_link.id}") do
+      expect(page).to have_content("Mark As Unread")
+      find("button#button#{read_link.id}")
     end
 
   end
