9cf6c31b90b43ff9a081adbc5876d133adbc6aa6
Anna Weisbrodt <aweisbro@gmail.com>
2016-09-20 16:27:48 -0600

    edit link url sad path

--- spec/features/authenticated_user_edits_link_spec.rb
+++ spec/features/authenticated_user_edits_link_spec.rb
@@ -1,34 +1,60 @@
 require "rails_helper"
 
 feature "authenticated user edits link" do
-  scenario "user sees updated link in index", js: true do
-    user = create(:user)
-    create_list(:link, 3, user_id: user.id)
-    target_link = create(:link, title: "Old Title", url: "https://www.yahoo.com", user_id: user.id)
+  context "with valid information" do
+    scenario "user sees updated link in index", js: true do
+      user = create(:user)
+      create_list(:link, 3, user_id: user.id)
+      target_link = create(:link, title: "Old Title", url: "https://www.yahoo.com", user_id: user.id)
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return( user )
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return( user )
 
-    visit links_path
+      visit links_path
 
-    within("ul#link#{target_link.id}") do
-      click_on "Edit"
+      within("ul#link#{target_link.id}") do
+        click_on "Edit"
+      end
+
+      expect(current_path).to eq edit_link_path(target_link.id)
+
+      expect(page).to have_content("Edit Link")
+
+      fill_in "Title", with: "Updated Title"
+      fill_in "Url", with: "https://www.google.com"
+      click_on "Update"
+
+      expect(current_path).to eq(links_path)
+
+      expect(page).to have_content "Updated Title"
+      expect(page).to have_content "https://www.google.com"
+
+      expect(page).to_not have_content "Old Title"
+      expect(page).to_not have_content "https://www.yahoo.com"
     end
+  end
 
-    expect(current_path).to eq edit_link_path(target_link.id)
+  context "with invalid information" do
+    scenario "user error message", js: true do
+      user = create(:user)
+      create_list(:link, 3, user_id: user.id)
+      target_link = create(:link, title: "Title", url: "https://www.yahoo.com", user_id: user.id)
 
-    expect(page).to have_content("Edit Link")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return( user )
 
-    fill_in "Title", with: "Updated Title"
-    fill_in "Url", with: "https://www.google.com"
-    click_on "Update"
+      visit links_path
 
-    expect(current_path).to eq(links_path)
+      within("ul#link#{target_link.id}") do
+        click_on "Edit"
+      end
 
-    expect(page).to have_content "Updated Title"
-    expect(page).to have_content "https://www.google.com"
+      expect(current_path).to eq edit_link_path(target_link.id)
 
-    expect(page).to_not have_content "Old Title"
-    expect(page).to_not have_content "https://www.yahoo.com"
+      expect(page).to have_content("Edit Link")
 
+      fill_in "Url", with: "https.gle.com"
+      click_on "Update"
+
+      expect(page).to have_content("Url is not a valid URL")
+    end
   end
 end
