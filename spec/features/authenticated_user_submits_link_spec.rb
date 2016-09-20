988bd298a9b9a269bfbd058f39b0ca63769860ce
Anna Weisbrodt <aweisbro@gmail.com>
2016-09-20 13:22:13 -0600

    invalid link submission fixed

--- spec/features/authenticated_user_submits_link_spec.rb
+++ spec/features/authenticated_user_submits_link_spec.rb
@@ -1,17 +1,35 @@
 require "rails_helper"
 
 feature "authenticated user submits link" do
-  scenario "user sees all user's links on index load", js: true do
-    user = create(:user)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return( user )
+  context "with valid information" do
+    scenario "user sees all user's links on index load", js: true do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return( user )
 
-    visit links_path
+      visit links_path
 
-    fill_in "Title", with: "NYT"
-    fill_in "Url", with: "https://www.nytimes.com"
-    click_on "Submit Link"
+      fill_in "Title", with: "NYT"
+      fill_in "Url", with: "https://www.nytimes.com"
+      click_on "Submit Link"
 
-    expect(page).to have_content "NYT"
-    expect(page).to have_content "https://www.nytimes.com"
+      expect(page).to have_content "NYT"
+      expect(page).to have_content "https://www.nytimes.com"
+    end
+  end
+
+  context "with invalid information" do
+    scenario "user enter invalid url, sees error message", js: true do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return( user )
+
+      visit links_path
+
+      fill_in "Title", with: "New Title"
+      fill_in "Url", with: "w.nytimes.com"
+      click_on "Submit Link"
+
+      expect(page).to_not have_content "New Title"
+      expect(page).to_not have_content "w.nytimes.com"
+    end
   end
 end
