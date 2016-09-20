76e7f7a50bba3d726411c8756bbf12ff2917563e
Anna Weisbrodt <aweisbro@gmail.com>
2016-09-20 07:52:23 -0600

    status update in progress checkbox click registering

--- app/assets/javascripts/change_link_status.js
+++ app/assets/javascripts/change_link_status.js
@@ -1,26 +1,26 @@
-$(document).ready(function(){
-
-  $('.status-box').on('click', function() {
-      var link = $(this).parents("div");
-      var linkClass = $(this).parents("div").attr("class");
-      var linkId = $(this).parents("div").attr("id");
-      var newLinkStatus = $(this).attr("value");
-      var data = {link: { read: newLinkStatus} };
-
-      $.ajax({
-        url: "/api/v1/links/" + linkId + ".json",
-        method: "PATCH",
-        dataType: "JSON",
-        data: data,
-        success: function(updatedLink){
-          $(".read").toggle("style");
-          $(".unread").toggle("style");
-          $('input:checkbox').removeAttr('checked');
-        },
-        error: function(xhr) {
-         console.log(xhr.responseText);
-       }
-      });
-     });
-
-  });
+// $(document).ready(function(){
+//
+//   $('.status-box').on('click', function() {
+//       var link = $(this).parents("div");
+//       var linkClass = $(this).parents("div").attr("class");
+//       var linkId = $(this).parents("div").attr("id");
+//       var newLinkStatus = $(this).attr("value");
+//       var data = {link: { read: newLinkStatus} };
+//
+//       $.ajax({
+//         url: "/api/v1/links/" + linkId + ".json",
+//         method: "PATCH",
+//         dataType: "JSON",
+//         data: data,
+//         success: function(updatedLink){
+//           $(".read").toggle("style");
+//           $(".unread").toggle("style");
+//           $('input:checkbox').removeAttr('checked');
+//         },
+//         error: function(xhr) {
+//          console.log(xhr.responseText);
+//        }
+//       });
+//      });
+//
+//   });
--- app/assets/javascripts/components/link.js.jsx
+++ app/assets/javascripts/components/link.js.jsx
@@ -3,12 +3,28 @@ class Link extends React.Component {
     super(props);
   }
 
+  updateStatus() {
+    var newStatus = $("input[type=checkbox]:checked").data("target");
+    var linkId = $("input[type=checkbox]:checked").attr("id");
+
+    var data = {link: { read: newStatus} };
+
+    $.ajax({
+        url: "/api/v1/links/" + linkId + ".json",
+        method: "PATCH",
+        data: data,
+          success:(updatedLink) => {
+            console.log("updated!")
+          },
+       });
+  }
+
   render() {
     if ( this.props.link.read ) {
       return(
       <ul className='links-listing read' id={'link' + this.props.link.id} data-target="read">
         <h4>Mark As Unread
-          <input type="checkbox" className="status-box" ref="true"/>
+          <input type="checkbox" onClick={this.updateStatus.bind(this)} id={this.props.link.id} className="status-box" data-target="true"/>
         </h4>
         <li className='title'>{this.props.link.title}</li>
         <li className='url'>{this.props.link.url}</li>
@@ -20,7 +36,7 @@ class Link extends React.Component {
       return(
       <ul className='links-listing unread' id={'link' + this.props.link.id} data-target="unread">
         <h4>Mark As Read
-          <input type="checkbox" className="status-box" ref="false"/>
+          <input type="checkbox" onClick={this.updateStatus.bind(this)} id={this.props.link.id} className="status-box" data-target="false"/>
         </h4>
         <li className='title'>{this.props.link.title}</li>
         <li className='url'>{this.props.link.url}</li>
