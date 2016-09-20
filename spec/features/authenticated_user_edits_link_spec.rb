8fb1fd00ce1f55d3e7b41d48dcd7869603a5c3cb
Anna Weisbrodt <aweisbro@gmail.com>
2016-09-20 12:56:53 -0600

    edit test sad path

--- spec/features/authenticated_user_edits_link_spec.rb
+++ spec/features/authenticated_user_edits_link_spec.rb
@@ -4,7 +4,7 @@ feature "authenticated user edits link" do
   scenario "user sees updated link in index", js: true do
     user = create(:user)
     create_list(:link, 3, user_id: user.id)
-    target_link = create(:link, user_id: user.id)
+    target_link = create(:link, title: "Old Title", url: "https://www.yahoo.com", user_id: user.id)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return( user )
 
@@ -26,6 +26,9 @@ feature "authenticated user edits link" do
 
     expect(page).to have_content "Updated Title"
     expect(page).to have_content "https://www.google.com"
+
+    expect(page).to_not have_content "Old Title"
+    expect(page).to_not have_content "https://www.yahoo.com"
 
   end
 end
